99e47e7ce5da16098a2a641dbded4946b9ebe508
mannypulator <emeritus93@gmail.com>
2020-03-22 11:40:08 +0100

    added javascript task

---
+++ Task 1/JavaScript Task/script.js
@@ -0,0 +1,10 @@
+var myName = "Emmanuel";
+var courses = ["HTML","CSS","JavaScript"];
+
+console.log(myName);
+console.log(courses);
+for(var i = 0; i <=200; i++){
+    if(i%2!=0){
+        console.log(i);
+    }
+}
